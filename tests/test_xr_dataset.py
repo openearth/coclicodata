14f059882dc7291843d3e3bc4cd29d49f022f692
FlorisCalkoen <floris_calkoen@hotmail.com>
2022-06-01 16:59:46 +0200

    dir for tests to validate stac and etl process

---
+++ tests/test_xr_dataset.py
@@ -0,0 +1,11 @@
+import xarray as xr
+
+
+def test_crs(ds):
+    """TODO: test if 'epsg' or 'proj:espg' in ds.coords or 'crs' in ds.attrs or spatial_ref
+    object in ds.coords/attrs.
+
+    Args:
+        ds (_type_): _description_
+    """
+    raise NotImplementedError
