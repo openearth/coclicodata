74171eb508d132185108acc4bc66eaf778aec44e
FlorisCalkoen <floris_calkoen@hotmail.com>
2022-08-04 17:38:20 +0200

    scrip to create all stacs

--- scripts/build-stacs.sh
+++ scripts/build-stacs.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# for file in "generate_stac_??.py"; do
-#     echo "RUN python $file"
-#     echo "Building STACs..."
-#     python $file
-#     echo "Done!"
-# done
-
 for file in generate_stac_??.py; do
     echo "RUN python $file"
     echo "Building STACs..."
