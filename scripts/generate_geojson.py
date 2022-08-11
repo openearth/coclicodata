535498368512beaacea630a0116705646934569c
FlorisCalkoen <floris_calkoen@hotmail.com>
2022-08-11 15:53:15 +0200

    added variable to mapbox layer name

--- scripts/generate_geojson.py
+++ scripts/generate_geojson.py
@@ -13,18 +13,11 @@ import geopandas as gpd
 import xarray as xr
 from etl import p_drive, rel_root
 from etl.cloud_services import dataset_from_google_cloud, geojson_to_mapbox
-from etl.extract import (
-    clear_zarr_information,
-    get_geojson,
-    get_mapbox_url,
-    zero_terminated_bytes_as_str,
-)
+from etl.extract import (clear_zarr_information, get_geojson, get_mapbox_url,
+                         zero_terminated_bytes_as_str)
 from etl.keys import load_env_variables, load_google_credentials
-from stac.utils import (
-    get_dimension_dot_product,
-    get_dimension_values,
-    get_mapbox_item_id,
-)
+from stac.utils import (get_dimension_dot_product, get_dimension_values,
+                        get_mapbox_item_id)
 
 if __name__ == "__main__":
     # hard-coded input params
@@ -33,20 +26,20 @@ if __name__ == "__main__":
     MAPBOX_PROJ = "global-data-viewer"
 
     # hard-coded input params at project level
-    DATASET_FILENAME = "europe_extreme_sea_level"
-    VARIABLES = ["eewl", "esl"]  # xarray variables in dataset
+    DATASET_FILENAME = "global_wave_energy_flux.zarr"
+    VARIABLES = ["wef"]  # xarray variables in dataset
 
     load_env_variables(env_var_keys=["MAPBOX_ACCESS_TOKEN"])
 
-    # # read data from gcs zarr store
-    # ds = dataset_from_google_cloud(
-    #     bucket_name=BUCKET_NAME, bucket_proj=BUCKET_PROJ, zarr_filename=DATASET_FILENAME
-    # )
+    # read data from gcs zarr store
+    ds = dataset_from_google_cloud(
+        bucket_name=BUCKET_NAME, bucket_proj=BUCKET_PROJ, zarr_filename=DATASET_FILENAME
+    )
 
-    import xarray as xr
+    # import xarray as xr
 
-    fpath = pathlib.Path.home().joinpath("ddata", "tmp", "CoastAlRisk_Europe_ESL.zarr")
-    ds = xr.open_zarr(fpath)
+    # fpath = pathlib.Path.home().joinpath("ddata", "tmp", "CoastAlRisk_Europe_ESL.zarr")
+    # ds = xr.open_zarr(fpath)
 
     ds = zero_terminated_bytes_as_str(ds)
 
