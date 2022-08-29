89b3063ff95401683771ebe18a0ab06b8e8dc10a
FlorisCalkoen <floris_calkoen@hotmail.com>
2022-08-29 14:52:18 +0200

    remove special characters from dataset

--- stac/utils.py
+++ stac/utils.py
@@ -3,11 +3,46 @@ import pathlib
 import re
 from copy import deepcopy
 from itertools import product
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Union
 
+import numpy as np
 import pystac
+import xarray as xr
 from pystac import Collection
 from pystac.extensions.datacube import DatacubeExtension, Dimension, Variable
+
+
+def rm_special_characters(
+    ds: xr.Dataset,
+    dimensions_to_check: List[str] = [],
+    characters: List[str] = [],
+) -> xr.Dataset:
+    """Remove special characters from coordinate axes.
+
+    Args:
+        ds (xr.Dataset): _description_
+        dimensions_to_check (List[str  |  None]): _description_
+        characters (List[str  |  None]): _description_
+
+    Returns:
+        xr.Dataset: _description_
+    """
+
+    # datasets are cf compliant, so strings will only be present in coordinate axes
+    for coord in ds.coords:
+        if coord in dimensions_to_check:
+            # only check axes that are strings (those will be in unicodes)
+            if ds.coords[coord].dtype.kind == np.dtype("U"):
+                # replace characters with empty strings
+                values = ds.coords[coord].str.replace(pat=characters, repl="").data
+                # use assign coords to preserve attributes of coordinate axis
+                ds = ds.assign_coords({coord: (coord, values, ds.coords[coord].attrs)})
+    return ds
+
+
+def filter_characters(v):
+    """Remove characters which are difficult to read in frontend from strings."""
+    return re.sub("[%]", "", v)
 
 
 def get_mapbox_item_id(dimdict: Dict[str, Any]) -> str:
@@ -23,10 +58,6 @@ def get_mapbox_item_id(dimdict: Dict[str, Any]) -> str:
 
 def get_dimension_values(ds, dimensions_to_ignore: List[str]) -> dict:
     """Dataset values per dimensions to dictionary."""
-
-    def filter_characters(v):
-        """Remove characters difficult to read in frontend from strings."""
-        return re.sub("[%]", "", v)
 
     coords = list(ds.coords)
     dims = list(ds.dims)
@@ -45,11 +76,6 @@ def get_dimension_values(ds, dimensions_to_ignore: List[str]) -> dict:
     # make a dictionary with the values per dimension
 
     dimvals = {dim: ds[dim].values.tolist() for dim in dims}
-
-    for dim, vals in dimvals.items():
-        vals = [filter_characters(v) for v in vals]
-        dimvals[dim] = vals
-
     return dimvals
 
 
