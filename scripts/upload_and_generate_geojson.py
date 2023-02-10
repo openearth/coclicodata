7223917e8c31856255c65b6f5d0a0558c069bb34
EtienneKras <62947009+EtienneKras@users.noreply.github.com>
2023-02-10 09:00:11 +0100

    Update upload_and_generate_geojson.py
    Enable script for usage with dataset 01, run in Windows OS

--- scripts/upload_and_generate_geojson.py
+++ scripts/upload_and_generate_geojson.py
@@ -33,14 +33,15 @@ if __name__ == "__main__":
     # hard-coded input params at project level
     coclico_data_dir = pathlib.Path(p_drive, "11205479-coclico", "data")
     dataset_dir = coclico_data_dir.joinpath("01_storm_surge_jrc")
-    DATASET_FILENAME = "shoreline_change_projections.zarr"
-    VARIABLES = ["sc"]
+    IN_FILENAME = "CoastAlRisk_Europe_EESSL.zarr" # original filename as on P drive
+    OUT_FILENAME = "europe_storm_surge_level.zarr" # file name in the cloud and on MapBox
+    VARIABLES = ["ssl"]
     ADDITIONAL_DIMENSIONS = [
-        "ensemble",
-        "scenarios",
+        "rp",
+        "nscenarios",
     ]
     # use these to reduce dimension, e.g., {ensemble: "mean", "time": [1995, 2020, 2100]}
-    MAP_SELECTION_DIMS = {"time": [2100]}
+    MAP_SELECTION_DIMS = {"nscenarios": [0, 1, 2], "rp": [5.0, 10.0, 20.0, 50.0, 100.0, 200.0, 500.0, 1000.0]}
 
     # TODO: safe cloud creds in password client 
     load_env_variables(env_var_keys=["MAPBOX_ACCESS_TOKEN"])
@@ -51,19 +52,19 @@ if __name__ == "__main__":
     # TODO: come up with checks for zarr data
 
     # upload data to gcs from local drive
-    source_data_fp = dataset_dir.joinpath(DATASET_FILENAME)
+    source_data_fp = dataset_dir.joinpath(IN_FILENAME)
 
     dataset_to_google_cloud(
         ds=source_data_fp,
         gcs_project=GCS_PROJECT,
         bucket_name=BUCKET_NAME,
         bucket_proj=BUCKET_PROJ,
-        zarr_filename=DATASET_FILENAME,
+        zarr_filename=OUT_FILENAME,
     )
 
     # read data from gcs 
     ds = dataset_from_google_cloud(
-        bucket_name=BUCKET_NAME, bucket_proj=BUCKET_PROJ, zarr_filename=DATASET_FILENAME
+        bucket_name=BUCKET_NAME, bucket_proj=BUCKET_PROJ, zarr_filename=OUT_FILENAME
     )
 
     # # read data from local source
@@ -75,8 +76,9 @@ if __name__ == "__main__":
     ds = zero_terminated_bytes_as_str(ds)
 
     # remove characters that cause problems in the frontend.
+
     ds = rm_special_characters(
-        ds, dimensions_to_check=ADDITIONAL_DIMENSIONS, characters=["%"]
+        ds=ds, dimensions_to_check=ADDITIONAL_DIMENSIONS, characters=["%"]
     )
 
     # This dataset has quite some dimensions, so if we would parse all information the end-user
@@ -102,12 +104,12 @@ if __name__ == "__main__":
         )
 
         # save feature collection as geojson in tempdir and upload to cloud
-        with pathlib.Path.home().joinpath("data", "tmp") as outdir:
+        with dataset_dir.joinpath("platform") as outdir:
             # with tempfile.TemporaryDirectory() as outdir:
 
             # TODO: put this in a function because this is also used in generate_stac scripts?
             mapbox_url = get_mapbox_url(
-                MAPBOX_PROJ, DATASET_FILENAME, var, add_mapbox_protocol=False
+                MAPBOX_PROJ, OUT_FILENAME, var, add_mapbox_protocol=False
             )
 
             fn = mapbox_url.split(".")[1]
@@ -122,4 +124,4 @@ if __name__ == "__main__":
 
             # Note, if mapbox cli raises en util collection error, this should be monkey
             # patched. Instructions are in documentation of the function.
-            # geojson_to_mapbox(source_fpath=fp, mapbox_url=mapbox_url)
+            geojson_to_mapbox(source_fpath=fp, mapbox_url=mapbox_url)
