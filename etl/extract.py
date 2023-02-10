ed8783dd0e53a172e3ba20a8ddbb3123bafdf3ad
EtienneKras <62947009+EtienneKras@users.noreply.github.com>
2023-02-10 08:56:08 +0100

    Update extract.py
    Fix bug in geojson creation

--- etl/extract.py
+++ etl/extract.py
@@ -93,8 +93,8 @@ def get_geojson(ds, variable, dimension_combinations, stations_dim):
     da = ds[variable]
     idxs = da[stations_dim].values.tolist()
 
-    # data with geometries can be read into geojson format directoy, but features from data
-    # wil no # geometry coordinates are constructed from lons/lats.
+    # data with geometries can be read into geojson format directly, but features from data
+    # with no geometry coordinates are constructed from lons/lats.
     if "geometry" in ds:
         features = [geojson.Feature(geometry=mapping(i)) for i in ds.geometry.values]
     else:
@@ -118,8 +118,8 @@ def get_geojson(ds, variable, dimension_combinations, stations_dim):
             if dim not in da.dims:
                 dimkey = "n" + dim
                 if dimkey in da.dims:
-                    da_ = da_.sel({dimkey: dim == dimdict.pop(dim)})
-
+                    da_ = da_.sel({dimkey: list(da_[dim].values).index(dimdict.pop(dim))})
+    
         vals = da_.sel(dimdict).values.tolist()
         for feature, value in zip(features, vals):
             feature["properties"][mapbox_layer_id] = value
