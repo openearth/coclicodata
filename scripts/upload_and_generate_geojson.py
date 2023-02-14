ef846d041d282ea5bd9f64da714c1cd586313a4a
EtienneKras <62947009+EtienneKras@users.noreply.github.com>
2023-02-14 13:10:26 +0100

    Update upload_and_generate_geojson.py

--- scripts/upload_and_generate_geojson.py
+++ scripts/upload_and_generate_geojson.py
@@ -136,4 +136,4 @@ if __name__ == "__main__":
 
             # Note, if mapbox cli raises en util collection error, this should be monkey
             # patched. Instructions are in documentation of the function.
-            # geojson_to_mapbox(source_fpath=fp, mapbox_url=mapbox_url)
+            geojson_to_mapbox(source_fpath=fp, mapbox_url=mapbox_url)
