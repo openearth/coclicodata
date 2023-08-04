03976832b0678c09066a61a3754f71d815ca6b0a
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2023-08-04 13:51:00 +0200

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,11 +1,11 @@
 from pystac import Collection, CatalogType
 from os import environ
 
-branch = environ.get("GITHUB_REF_NAME", "live")
+branch = environ.get("GITHUB_REF_NAME", "test-groups")
 
-href = "https://raw.githubusercontent.com/openearth/coclico/live/"
+href = "https://raw.githubusercontent.com/openearth/coclico/test-groups/"
 
 collection = Collection.from_file("./current/collection.json")
 collection.describe()
 collection.normalize_hrefs(href)
-collection.save(catalog_type=CatalogType.ABSOLUTE_PUBLISHED, dest_href="live")
+collection.save(catalog_type=CatalogType.ABSOLUTE_PUBLISHED, dest_href="test-groups")
