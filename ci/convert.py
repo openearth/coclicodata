226b38a5093d6228e89d4ba5123a35eedc254778
floriscalkoen <floris_calkoen@hotmail.com>
2023-08-30 12:32:46 +0200

    make dir for ci scripts; add convert.py to this dir.

---
+++ ci/convert.py
@@ -0,0 +1,12 @@
+from pystac import Collection, CatalogType
+from os import environ
+from pystac import Catalog
+
+branch = environ.get("GITHUB_REF_NAME", "live")
+
+href = "https://raw.githubusercontent.com/openearth/coclicodata/live/current/"
+
+collection = Catalog.from_file("./current/catalog.json")
+collection.describe()
+collection.normalize_hrefs(href)
+collection.save(catalog_type=CatalogType.ABSOLUTE_PUBLISHED, dest_href="live")
