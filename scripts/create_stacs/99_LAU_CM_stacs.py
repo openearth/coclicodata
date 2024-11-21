f302099dd2914c78e78336498fa962ba12e97ec0
mathvansoest <mathvansoest@gmail.com>
2024-11-21 10:09:07 +0100

    Added Coastal Mask LAU to STAC, revisions still required.

--- scripts/create_stacs/99_LAU_CM_stacs.py
+++ scripts/create_stacs/99_LAU_CM_stacs.py
@@ -50,15 +50,15 @@ GCS_PROTOCOL = "https://storage.googleapis.com"
 GCS_PROJECT = "coclico-11207608-002"
 BUCKET_NAME = "coclico-data-public"
 BUCKET_PROJ = "coclico"
-PROJ_NAME = "NUTS"
+PROJ_NAME = "LAU"
 
 # hard-coded STAC templates
 STAC_DIR = pathlib.Path.cwd().parent.parent / "current"
 
 # hard-coded input params which differ per dataset
-DATASET_DIR = "XX_NUTS"
+DATASET_DIR = "XX_LAU"
 # CF_FILE = "Global_merit_coastal_mask_landwards.tif"
-COLLECTION_ID = "NUTS"  # name of stac collection
+COLLECTION_ID = "LAU"  # name of stac collection
 MAX_FILE_SIZE = 500  # max file size in MB
 
 # define local directories
@@ -82,8 +82,8 @@ if not ds_dir.exists():
 
 # # directory to export result
 # cog_dirs = ds_dir.joinpath("cogs")
-ds_path = ds_dir.joinpath("XX_NUTS")
-ds_fp = ds_path.joinpath("NUTS_RG_01M_2021_3035.parquet")  # file directory
+ds_path = ds_dir.joinpath("XX_LAU")
+ds_fp = ds_path.joinpath("LAU_2020_NUTS_2021_01M_3035.parquet")  # file directory
 
 # # load metadata template
 metadata_fp = ds_path.joinpath("metadata", ds_fp.name).with_suffix(".json")
