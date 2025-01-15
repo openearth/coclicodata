d9fc83ed171c754a8ce8fbbdef893140e7e6e862
EtienneKras <62947009+EtienneKras@users.noreply.github.com>
2025-01-15 08:07:31 +0100

    remove geoserver link from CBA stats

--- scripts/create_stacs/33_cba_stats_stacs.py
+++ scripts/create_stacs/33_cba_stats_stacs.py
@@ -260,14 +260,14 @@ def create_collection(
         ),
     )
 
-    collection.add_asset(
-        "geoserver_link",
-        pystac.Asset(
-            "https://coclico.avi.deltares.nl/geoserver/gwc/service/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&LAYER=cba:GCF_open_CBA_country_all_EPSG4326&STYLE=&TILEMATRIX=EPSG:900913:{z}&TILEMATRIXSET=EPSG:900913&FORMAT=application/vnd.mapbox-vector-tile&TILECOL={x}&TILEROW={y}",
-            title="Geoserver Parquet link",
-            media_type="application/vnd.apache.parquet",
-        ),
-    )
+    # collection.add_asset(
+    #     "geoserver_link",
+    #     pystac.Asset(
+    #         "https://coclico.avi.deltares.nl/geoserver/gwc/service/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&LAYER=cba:GCF_open_CBA_country_all_EPSG4326&STYLE=&TILEMATRIX=EPSG:900913:{z}&TILEMATRIXSET=EPSG:900913&FORMAT=application/vnd.mapbox-vector-tile&TILECOL={x}&TILEROW={y}",
+    #         title="Geoserver Parquet link",
+    #         media_type="application/vnd.apache.parquet",
+    #     ),
+    # )
 
     # collection.links = links
     collection.keywords = metadata["KEYWORDS"]
