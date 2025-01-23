9171f517af40783b0611de97f43b6379ab053d96
EtienneKras <62947009+EtienneKras@users.noreply.github.com>
2025-01-23 07:38:11 +0100

    Fix EESL

--- scripts/create_stacs/03_eesl_stacs.py
+++ scripts/create_stacs/03_eesl_stacs.py
@@ -6,7 +6,11 @@ from posixpath import join as urljoin
 
 import pystac
 from coclicodata.drive_config import p_drive
-from coclicodata.etl.cloud_utils import dataset_from_google_cloud
+from coclicodata.etl.cloud_utils import (
+    dataset_from_google_cloud,
+    dataset_to_google_cloud,
+    load_google_credentials,
+)
 from coclicodata.etl.extract import get_mapbox_url, zero_terminated_bytes_as_str
 from pystac import Catalog, CatalogType, Collection, Summaries
 
@@ -35,6 +39,7 @@ from coclicodata.coclico_stac.utils import (
 
 if __name__ == "__main__":
     # hard-coded input params at project level
+    GCS_PROJECT = "CoCliCo - 11207608-002"
     BUCKET_NAME = "coclico-data-public"
     BUCKET_PROJ = "coclico"
     MAPBOX_PROJ = "global-data-viewer"
@@ -46,6 +51,7 @@ if __name__ == "__main__":
     DATASET_DESCRIPTION = """The Extreme Sea Level (ESL) dataset represents the distribution of the total water level (TWL) design conditions at European scale. Both the ESL and the Episodic Extreme Water Level (EEWL, extreme sea level including storm surge level and wave height) are estimated for two climate scenarios (RCP4.5 and RCP8.5) as well as eight return periods (5, 10, 20, 50, 100, 200, 500 and 1000). This dataset is part of the [LISCOAST](https://data.jrc.ec.europa.eu/collection/LISCOAST) project. See this [article](https://doi.org/10.1002/2016EF000505) for more dataset-specific information."""
 
     # hard-coded input params which differ per dataset
+    DATASET_INFILENAME = "CoastAlRisk_Europe_ESL.zarr"
     DATASET_FILENAME = "europe_extreme_sea_level.zarr"
     VARIABLES = ["eewl", "esl"]  # xarray variables in dataset
     X_DIMENSION = "lon"  # False, None or str; spatial lon dim used by datacube
@@ -111,6 +117,24 @@ if __name__ == "__main__":
     gcs_zarr_store = os.path.join("gcs://", BUCKET_NAME, BUCKET_PROJ, DATASET_FILENAME)
     gcs_api_zarr_store = os.path.join(
         "https://storage.googleapis.com", BUCKET_NAME, BUCKET_PROJ, DATASET_FILENAME
+    )
+
+    # add file to bucket
+    cred_data_dir = p_drive.joinpath("11207608-coclico", "FASTTRACK_DATA")
+    # load google credentials
+    load_google_credentials(
+        google_token_fp=cred_data_dir.joinpath("google_credentials_new.json")
+    )
+    coclico_data_dir = p_drive.joinpath("11207608-coclico", "FASTTRACK_DATA")
+    dataset_dir = coclico_data_dir.joinpath("03_sea_level_jrc")
+    source_data_fp = dataset_dir.joinpath(DATASET_INFILENAME)
+
+    dataset_to_google_cloud(
+        ds=source_data_fp,
+        gcs_project=GCS_PROJECT,
+        bucket_name=BUCKET_NAME,
+        bucket_proj=BUCKET_PROJ,
+        zarr_filename=DATASET_FILENAME,
     )
 
     # read data from gcs zarr store
