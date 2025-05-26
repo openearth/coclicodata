612cbd86ff3ce6d588a900da0d52b1c6c36e1a3d
EtienneKras <62947009+EtienneKras@users.noreply.github.com>
2025-05-26 15:10:46 +0200

    Add Zarr to EDITO storage

---
+++ scripts/utils/migrate_data_to_EDITO.py
@@ -0,0 +1,68 @@
+# %%
+# written by Etienne Kras, 19-05-2025
+# This script migrates data from a local zarr store to the EDITO S3 storage
+# sources: https://datalab.dive.edito.eu/account/storage and https://pub.pages.mercator-ocean.fr/edito-infra/edito-tutorials-content/#/zarr-netcdf-xarray-edito-data-storage
+
+# import packages
+from os import environ
+import xarray as xr
+from xarray import Dataset
+import s3fs
+import pathlib
+
+# import polars as pl
+
+# define user
+USERNAME = "project-coclico"
+
+# set environment variables
+fs = s3fs.S3FileSystem(
+    client_kwargs={"endpoint_url": "https://" + "minio.dive.edito.eu"},
+    key=environ["EDITO_AWS_ACCESS_KEY_ID"],
+    secret=environ["EDITO_AWS_SECRET_ACCESS_KEY"],
+    token=environ["EDITO_AWS_SESSION_TOKEN"],
+)
+
+# define (local and) remote drives
+coclico_data_dir = pathlib.Path("P:/").joinpath(
+    "11207608-coclico",
+    "FULLTRACK_DATA",
+    "WP3",
+    "data",
+    "NetCDF_MarineDynamicsChanges_TWL",
+)
+# TODO: get data from Google bucket and write to EDITO S3 storage (MiniO)
+
+
+# load dataset (Zarr, Parquet, CoG or NetCDF)
+
+# Zarr example
+dataset_name = "CTP_ReturnPeriods_SLR"
+dataset = xr.open_zarr(coclico_data_dir.joinpath("%s.zarr" % (dataset_name)))
+# variable_names = [var for var in dataset.variables if var not in dataset.coords]
+
+# Parquet example
+# ??
+
+# CoG example
+# ??
+
+# NetCDF example
+# see https://pub.pages.mercator-ocean.fr/edito-infra/edito-tutorials-content/#/writing-data-on-the-fly-to-edito-data-storage?id=uploading-netcdf-files
+
+# general example
+# dataset: Dataset = (
+#     ...
+# )  # a dataset containing values of a variable my_var over dimensions x and y
+# x_chunk: int = ...  # maximum number of value in each chunk over dimension x
+# y_chunk: int = ...  # maximum number of value in each chunk over dimension y
+# encoding = {"my_var": {"chunks": (x_chunk, y_chunk)}}
+
+# write output to my files in EDITO's S3 storage
+out_store = s3fs.S3Map(
+    root=f"%s/%s.zarr" % (USERNAME, dataset_name), s3=fs, create=True
+)
+dataset.to_zarr(store=out_store, consolidated=True, mode="w")  # encoding=encoding)
+
+# test written output
+# test = xr.open_dataset("https://minio.dive.edito.eu/oidc-%s/%s.zarr"%(USERNAME, dataset_name), engine="zarr")
